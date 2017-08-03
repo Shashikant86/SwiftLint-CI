7bfe2f0ea882ce25cf6bb2d5aedc9cf1130ad5e0
Shashikant86 <shashikant.jagtap@icloud.com>
2017-08-03 23:16:20 +0100

    Initial Commit

---
+++ SwiftLint-CI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLint-CI
+//
+//  Created by Shashikant Jagtap on 03/08/2017.
+//  Copyright © 2017 Shashikant Jagtap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
